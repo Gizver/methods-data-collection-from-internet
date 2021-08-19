7c20d9b0908ccca01a258ec4d158fa74dad5c212
Gizver <74718101+Gizver@users.noreply.github.com>
2021-08-19 19:22:03 +0300

    Update Homework_1_2.py

--- Homework_1_2.py
+++ Homework_1_2.py
@@ -7,7 +7,7 @@ import requests, json
 
 def lastfm_get(payload):
 
-    headers = {'user-agent': 'Boris Mukhin'}
+    headers = {'USER-AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.135 YaBrowser/21.6.2.855 Yowser/2.5 Safari/537.36'}
     url = 'https://ws.audioscrobbler.com/2.0/'
 
     payload['api_key'] = 'a0460860e28c2efaa078e8a8f6182801'
@@ -18,13 +18,13 @@ def lastfm_get(payload):
 
 
 #Используем метод artist.getTopTags, для получения топа тегов исполнителя
-r = lastfm_get({
+top_teg = lastfm_get({
     'method': 'artist.getTopTags',
     'artist':  'Slayer'
 })
 
 #Получим топ 6 тегов для группы 'Slayer'
-tags = [t['name'] for t in r.json()['toptags']['tag'][:6]]
+tags = [t['name'] for t in top_teg.json()['toptags']['tag'][:6]]
 print(tags)
 
 with open('slayer_tags.json', 'w') as outfile:
