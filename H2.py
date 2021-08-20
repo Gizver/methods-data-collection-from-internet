09295df04d696eb581bdb6241db7db4be1beb8ab
Gizver <74718101+Gizver@users.noreply.github.com>
2021-08-21 02:28:22 +0300

    Update H2.py

--- H2.py
+++ H2.py
@@ -7,96 +7,103 @@
 
 
 
-from bs4 import BeautifulSoup
-import lxml
-import pandas as pd
+from bs4 import BeautifulSoup as bs
 import requests
-from fake_headers import Headers
 import re
+from pprint import pprint
+import json
+
+my_header = {'USER-AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.135 YaBrowser/21.6.2.855 Yowser/2.5 Safari/537.36'}
 
 
-headers = Headers(headers=True).generate()
+vacancy = 'python'
 
-def hh_parser(vacancy='analitik', pages=3):
-    data = []
-    for i in range(pages):
-        url = 'https://spb.hh.ru/vacancies/{}?page={}'.format(vacancy, i)
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'lxml')
-        vacancy_data = soup.findAll('div', class_='vacancy-serp-item')
+params = {
+        'text': vacancy,
+        'search_field': 'name'
+    }
+
+url = 'https://hh.ru/search/vacancy'
+response = requests.get(url, params=params, headers=my_header)
+soup = bs(response.text, 'html.parser')
+
+#сделаем вариант со сбором инфы с первой по последнюю страницу
+
+next_page_blok = soup.find('div', {'data-qa': 'pager-block'}) #находим блок с кнопками перехода на следующие страницы
+
+#подтверждаем его существование
+if next_page_blok:
+    last_page_number = int(next_page_blok.find_all('a', {'class':'bloko-button', 'data-qa': 'pager-page'})[-1].getText()) - 1   #находим последнюю возможную страницу
+                                                                                                                                # и так как на HH нумерация с 0, то отнимаем 1
+else:
+    last_page_number = 1
+data = []
+#перебираем страницы и собираем их код
+for page in range(0, last_page_number):
+
+    params['page'] = page
+    response = requests.get(url, params, headers=my_header)
 
 
-        for i in vacancy_data:
-            name = i.find('a', class_='bloko-link').text
-            link = i.find('a', class_='bloko-link')['href']
+    soup_page = bs(response.text, 'html.parser')
+    vacancy_data = soup_page.findAll('div', class_='vacancy-serp-item')
 
-            salary = i.find('div', class_='vacancy-serp-item__sidebar').text
-            if not salary:
-                min = None
+
+
+    for i in vacancy_data:
+        name = i.find('a', class_='bloko-link').text
+        link = i.find('a', class_='bloko-link')['href']
+
+        #создаем словарь с зарплатами обрабатывая варианты с отсутствием какой-либо инфы
+        salary = i.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+        if not salary:
+            min = None
+            max = None
+            money_name = None
+        else:
+            salary = salary.text
+            salary = re.split(r'\s|-', salary)
+#так как split разделил зарплаты по пробелу, то чтобы не возиться с регулярками стал проверять некоторые элементы списка на то, число они или нет
+            if salary[0] == 'от':
+                if salary[2].isnumeric():
+                    min = int(salary[1] + salary[2])
+                else:
+                    min = int(salary[1])
+
                 max = None
-            else:
-                salary = re.split(r'\s|-', salary)
-                if salary[0] == 'от':
-                    min = salary[1] + salary[2]
-                    max = None
-                elif salary[0] == 'до':
-                    min = None
-                    max = salary[1] + salary[2]
+
+            elif salary[0] == 'до':
+                min = None
+
+                if salary[2].isnumeric():
+                    max = int(salary[1] + salary[2])
                 else:
-                    min = salary[0] + salary[1]
-                    max = salary[3] + salary[4]
+                    max = int(salary[1])
 
-            data.append({
-                'Vacancy name ': name,
-                'Min salary': min,
-                'Max salary': max,
-                'URL': link
-            })
-    return data
-
-
-
-def s_job_parser(vacancy='analitik', pages=2):
-    data = []
-    for i in range(pages):
-        url = 'https://spb.superjob.ru/vakansii/{}.html?page={}'.format(vacancy, pages)
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'lxml')
-        vacancy_data = soup.find_all('div', {'class': 'f-test-vacancy-item'})
-
-
-        for i in vacancy_data:
-            name = i.find('a').text
-            link = 'https://spb.superjob.ru' + i.find('a')['href']
-
-            salary = i.find('span', class_='_1h3Zg _2Wp8I _2rfUm _2hCDz _2ZsgW').text
-
-
-            if salary == 'По договорённости':
-                min = max = None
             else:
-                salary = re.split(r'\s|-', salary)
-                if salary[0] == 'от':
-                    min = salary[1] + salary[2]
-                    max = None
-                elif salary[0] == 'до':
-                    min = None
-                    max = salary[1] + salary[2]
-                elif '-' not in salary:
-                    min = max = salary[0] + salary[1]
+                if salary[1].isnumeric():
+                    min = int(salary[0] + salary[1])
                 else:
-                    min = salary[0] + salary[1]
-                    max = salary[3] + salary[4]
-
-            data.append({
-                    'Vacancy name ': name,
-                    'Min salary': min,
-                    'Max salary': max,
-                    'URL': link
-                })
-        return data
+                    min = int(salary[0])
+                if salary[4] .isnumeric():
+                    max = int(salary[3] + salary[4])
+                else:
+                    max = int(salary[3])
 
 
-hh_s_job_data = hh_parser() + s_job_parser()
-df = pd.DataFrame(hh_s_job_data)
-print(df)
+            money_name = salary[-1]
+
+
+        data.append({
+            'Vacancy name ': name,
+            'Min salary': min,
+            'Max salary': max,
+            'Сurrency': money_name,
+            'URL': link
+        })
+
+pprint(data)
+print(f'С {last_page_number + 1} страниц было собрано {len(data)} вакансий')
+
+with open('hh_vacansy.json', 'w', encoding='UTF-8') as outfile:
+    json.dump(data, outfile, ensure_ascii=False, separators=(',', ': '))
