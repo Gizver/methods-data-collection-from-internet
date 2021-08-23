98fcec7474102adfe0f0dd14d00ac7bbf0baf555
Gizver <74718101+Gizver@users.noreply.github.com>
2021-08-24 01:02:02 +0300

    Update Homework_3.py

--- Homework_3.py
+++ Homework_3.py
@@ -7,52 +7,31 @@ client = MongoClient('localhost', 27017)
 
 db = client['vacancy']
 
-collection = db['analitik']
+collection = db['python']
 
 # В data.json хранятся собранные вакансии (ДЗ №2)
-with open('data.json') as f:
+with open('data.json', encoding='UTF-8') as f:
     file_data = json.load(f)
 
 collection.insert_many(file_data)
 
 # 2. Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой больше введённой суммы.
 
-def find_document(collection, salary):
+def find_document(collection, salary, currency):
 
-    results = collection.find({'$or': [{'Min salary': {'$gt': salary}}, {'Max salary': {'$gt': salary}}]})
+    results = collection.find({'$or': [{'Min salary': {'$gt': salary}}, {'Max salary': {'$gt': salary}}]}, {'Сurrency':currency})
     return [r for r in results]
 
 
 
-result = find_document(collection, 90000)
+result = find_document(collection, 90000, 'руб.')
 print(result)
 
 
 # 3. Написать функцию, которая будет добавлять в вашу базу данных только новые вакансии с сайта.
 
 def new_vacancy(data):
+    for el in data:
+        collection.update_one({'URL': el['URL']}, {'$set': el}, upsert=True)
 
-    # Соберем список ссылок на вакансии из нашей БД
-    url_list = []
-
-    res = collection.find({})
-
-    for r in res:
-        url_list.append(r['URL'])
-
-
-    # Выгрузим информацию о вакансиях из json файла
-    with open('{}.json'.format(data)) as f:
-        file_data = json.load(f)
-
-
-    new_url = []
-    for el in file_data:
-        if el['URL'] not in url_list:
-            new_url.append(el)
-
-    collection.insert_many(new_url)
-    print(f'Было добавлено {len(new_url)} записи')
-
-
-new_vacancy('data')
+new_vacancy(file_data)
