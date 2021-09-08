3ec7b664c4bf4c759f798b6dbe362cadb00c190e
Gizver <74718101+Gizver@users.noreply.github.com>
2021-09-08 19:47:11 +0300

    Update items.py

--- items.py
+++ items.py
@@ -8,7 +8,7 @@ from itemloaders.processors import MapCompose, TakeFirst
 from w3lib.html import remove_tags
 import re
 
-def str_to_float(value):
+def try_str_to_float(value):
     try:
         value = float(value.replace(' ', ''))
     except ValueError:
@@ -21,20 +21,20 @@ def remove_spaces_in_options(value):
         for k, v in value.items():
             v = v.lstrip()
             v = v.rstrip()
-            value[k] = str_to_float(v)
+            value[k] = try_str_to_float(v)
 
     return value
 
 
-def link_to_big_photo(value):
+def remove_size_from_ph_link(value):
     return re.sub(",w_\d*,h_\d*,",",", value)
 
 
-class GoodsItem(scrapy.Item):
+class HomegoodsItem(scrapy.Item):
     name = scrapy.Field(output_processor=TakeFirst())
-    photos = scrapy.Field(input_processor=MapCompose(link_to_big_photo))
+    photos = scrapy.Field(input_processor=MapCompose(remove_size_from_ph_link))
     description = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     options = scrapy.Field(input_processor=MapCompose(remove_spaces_in_options), output_processor=TakeFirst())
     link = scrapy.Field(output_processor=TakeFirst())
-    price = scrapy.Field(input_processor=MapCompose(str_to_float), output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(try_str_to_float), output_processor=TakeFirst())
     _id = scrapy.Field()
